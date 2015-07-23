2a698dd6c8a44918edfd2bd6b9474c1fa21e1779
lnikod4s <lyudmil.nikodimov@gmail.com>
2015-07-23 17:22:22 +0300

    Added new utils class

---
+++ public/assets/js/utils.js
@@ -0,0 +1,14 @@
+var utils = {
+	comparePositions: function(pos1, pos2) {
+		return pos1.x == pos2.x && pos1.y == pos2.y;
+	},
+	removeFromArray: function(array, item) {
+		for (var i = 0, len = array.length; i < len; i++) {
+			if (item == array[i]) {
+				array.splice(i, 1);
+			}
+		}
+
+		return array;
+	}
+};
